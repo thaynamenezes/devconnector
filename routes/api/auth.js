84dd2db6e38b3d43a52d590a907fde829a2b0b86
Thayna Santana <thaynamenezes01@gmail.com>
2019-11-21 17:34:15 -0500

    middleware and route changes auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
 
 // @route           GET api/auth
 // @description     Test route
 // @access          Public
-router.get('/', (req, res) => res.send('Auth route'));
+
+// To protect route just add middleware as a param
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(error.message);
+    res.statu(500).send('Server Error');
+  }
+});
 
 module.exports = router;
